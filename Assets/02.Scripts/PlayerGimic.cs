550ac78b025138945c23628f5a08335bf88da0a8
kkc <94228190+arumolru@users.noreply.github.com>
2024-06-05 15:16:44 +0900

    기믹 제작중
    닿으면 게임오버되는 기믹 구현
    닿으면 위로 타고 올라가는 기믹 구현

--- Assets/02.Scripts/PlayerGimic.cs
+++ Assets/02.Scripts/PlayerGimic.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class PlayerGimic : MonoBehaviour
 {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("TRAPWALL"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("UPWALL"))
+        {
+            transform.Translate(transform.position + Vector3.up);
+        }
+    }
 }
