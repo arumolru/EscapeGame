a1e2fe12093ef7b718769be0b7aaee7c087eb2a4
kkc <94228190+arumolru@users.noreply.github.com>
2024-06-24 12:17:36 +0900

    모바일에 맞게 수정 작업 진행 중

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCtrl : MonoBehaviour
 {
@@ -8,31 +9,21 @@ public class PlayerCtrl : MonoBehaviour
 
     private float rotateSpeed = 100f; // 플레이어의 회전 속도
 
+    public VirtualJoystick joystick; // 가상 조이스틱
+
     private void FixedUpdate()
     {
         Move();
-
-        Turn();
     }
 
     void Move()
     {
         // 플레이어 이동 코드
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        float h = joystick.Horizontal();
+        float v = joystick.Vertical();
 
         Vector3 vec = new Vector3(Time.deltaTime * moveSpeed * h, 0f, Time.deltaTime * moveSpeed * v);
 
         transform.Translate(vec);
     }
-
-    void Turn()
-    {
-        // 플레이어의 회전 코드
-        float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
-
-        Vector3 rotate = new Vector3(0, Time.deltaTime * rotateSpeed * mouseX, 0);
-
-        transform.Rotate(rotate);
-    } 
 }
